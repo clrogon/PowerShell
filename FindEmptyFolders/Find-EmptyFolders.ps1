64cb5343cf11c804849bd6358ca0bf1a2e3e6785
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2023-08-02 18:05:40 +0100

    Update Find-EmptyFolders.ps1

--- FindEmptyFolders/Find-EmptyFolders.ps1
+++ FindEmptyFolders/Find-EmptyFolders.ps1
@@ -1,1 +1,29 @@
+function Find-EmptyFolders {
+    param (
+        [string]$FolderPath = "C:\",
+        [string]$LogFile = "C:\TSTFolder\Logs\EmptyFolders_log.log",
+        [string]$EmptyFoldersLog = "C:\TSTFolder\Logs\EmptyFolders_report.log"
+    )
 
+    # If the report file already exists, rename it with the last modified timestamp
+    if (Test-Path -Path $EmptyFoldersLog) {
+        $LastWriteTime = (Get-Item $EmptyFoldersLog).LastWriteTime.ToString('yyyyMMdd_HHmmss')
+        Rename-Item -Path $EmptyFoldersLog -NewName ("{0}_{1}.log" -f $EmptyFoldersLog.TrimEnd('.log'), $LastWriteTime)
+    }
+
+    Get-ChildItem -Path $FolderPath -Recurse -Directory | ForEach-Object {
+        try {
+            $files = Get-ChildItem -Path $_.FullName -Recurse -File -ErrorAction Stop
+            if (!$files) {
+                $_.FullName | Out-File -FilePath $EmptyFoldersLog -Append
+            }
+        }
+        catch {
+            $errorMessage = "Failed to access $($_.FullName): $($_.Exception.Message)"
+            Write-Error $errorMessage
+            $errorMessage | Out-File -FilePath $LogFile -Append
+        }
+    }
+}
+
+Find-EmptyFolders
